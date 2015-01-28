3428c80e9e282daf9d0efc96632e1e308000590e
Michael Wallner <mike@php.net>
2015-01-28 09:23:59 +0100

    integrade raphf

--- main/php_raphf.h
+++ main/php_raphf.h
@@ -1,38 +1,25 @@
 /*
-    +--------------------------------------------------------------------+
-    | PECL :: raphf                                                      |
-    +--------------------------------------------------------------------+
-    | Redistribution and use in source and binary forms, with or without |
-    | modification, are permitted provided that the conditions mentioned |
-    | in the accompanying LICENSE file are met.                          |
-    +--------------------------------------------------------------------+
-    | Copyright (c) 2013, Michael Wallner <mike@php.net>                 |
-    +--------------------------------------------------------------------+
-*/
+   +----------------------------------------------------------------------+
+   | PHP Version 7                                                        |
+   +----------------------------------------------------------------------+
+   | Copyright (c) 1997-2015 The PHP Group                                |
+   +----------------------------------------------------------------------+
+   | This source file is subject to version 3.01 of the PHP license,      |
+   | that is bundled with this package in the file LICENSE, and is        |
+   | available through the world-wide-web at the following url:           |
+   | http://www.php.net/license/3_01.txt                                  |
+   | If you did not receive a copy of the PHP license and are unable to   |
+   | obtain it through the world-wide-web, please send a note to          |
+   | license@php.net so we can mail you a copy immediately.               |
+   +----------------------------------------------------------------------+
+   | Authors: Michael Wallner <mike@php.net>                              |
+   +----------------------------------------------------------------------+
+ */
 
 #ifndef PHP_RAPHF_H
 #define PHP_RAPHF_H
 
-#ifndef DOXYGEN
-
-extern zend_module_entry raphf_module_entry;
-#define phpext_raphf_ptr &raphf_module_entry
-
-#define PHP_RAPHF_VERSION "2.0.0dev"
-
-#ifdef PHP_WIN32
-#	define PHP_RAPHF_API __declspec(dllexport)
-#elif defined(__GNUC__) && __GNUC__ >= 4
-#	define PHP_RAPHF_API extern __attribute__ ((visibility("default")))
-#else
-#	define PHP_RAPHF_API extern
-#endif
-
-#ifdef ZTS
-#	include "TSRM.h"
-#endif
-
-#endif
+#define PHP_RAPHF_API PHPAPI
 
 /**
  * A resource constructor.
